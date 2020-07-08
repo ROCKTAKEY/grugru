8cf3c7e397a0d31ba414abc406806ff417fd9d4a
ROCKTAKEY <rocktakey@gmail.com>
2020-07-08 23:20:11 +0900

    Remove package-requires of cl-lib.

--- grugru.el
+++ grugru.el
@@ -5,8 +5,8 @@
 ;; Author: ROCKTAKEY <rocktakey@gmail.com>
 ;; Keywords: convenience, abbrev, tools
 
-;; Version: 1.1.10
-;; Package-Requires: ((cl-lib "0.6.1") (emacs "24.4"))
+;; Version: 1.1.11
+;; Package-Requires: ((emacs "24.4"))
 ;; URL: https://github.com/ROCKTAKEY/grugru
 
 ;; This program is free software; you can redistribute it and/or modify
