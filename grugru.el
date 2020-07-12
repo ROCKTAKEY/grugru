ba6d504530fb169e23c752ab1927671479b6751d
ROCKTAKEY <rocktakey@gmail.com>
2020-07-12 16:37:33 +0900

    Update version.

--- grugru.el
+++ grugru.el
@@ -5,7 +5,7 @@
 ;; Author: ROCKTAKEY <rocktakey@gmail.com>
 ;; Keywords: convenience, abbrev, tools
 
-;; Version: 1.6.0
+;; Version: 1.6.1
 ;; Package-Requires: ((emacs "24.4"))
 ;; URL: https://github.com/ROCKTAKEY/grugru
 
