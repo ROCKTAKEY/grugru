72bda01e89e5b72e1dd78b2eeb519f9ab31d5da9
conao3 <conao3@gmail.com>
2020-08-02 17:17:16 +0900

    tagged v1.9.2

--- grugru.el
+++ grugru.el
@@ -5,7 +5,7 @@
 ;; Author: ROCKTAKEY <rocktakey@gmail.com>
 ;; Keywords: convenience, abbrev, tools
 
-;; Version: 1.9.1
+;; Version: 1.9.2
 ;; Package-Requires: ((emacs "24.4"))
 ;; URL: https://github.com/ROCKTAKEY/grugru
 
