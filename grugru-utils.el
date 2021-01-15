d8dce7c80c22e7a794207d222950482ed5a6cf56
ROCKTAKEY <rocktakey@gmail.com>
2021-01-15 16:31:26 +0900

    feat: Add grugru-utils-lisp-exchange-args.

--- grugru-utils.el
+++ grugru-utils.el
@@ -23,5 +23,37 @@
 
 ;;; Code:
 
+(require 'cl-lib)
+
+(defun grugru-utils-lisp-exchange-args (list-string permutation)
+  "Permute argument of sexp read from LIST-STRING according to PERMUTATION.
+
+For example, (grugru-utils-lisp-exchange-args \"(nth 1 '(x y z))\" '(2 1))
+returns \"(nth '(x y z) 1)\".  Newlines and whitespaces are also kept.
+
+This function is defined for user to define the function for grugru which rotate
+not only fuction's name but also arguments' order."
+  (ignore-errors
+    (let* ((no-parenthesis (substring-no-properties list-string 1 -1))
+           (function-name-pos (cdr (read-from-string no-parenthesis)))
+           (args-string (substring-no-properties
+                         no-parenthesis function-name-pos))
+           (args-string-list
+            (let ((pos 0)
+                  new-pos result)
+              (while (setq new-pos (ignore-errors (cdr (read-from-string args-string pos))))
+                (push (substring-no-properties args-string pos new-pos) result)
+                (setq pos new-pos))
+              (nreverse result)))
+           (permutation-length (length permutation))
+           (result (make-vector permutation-length nil)))
+      (cl-mapcar
+       (lambda (arg num) (setf (aref result (1- num)) arg))
+       args-string-list permutation)
+      (apply #'concat
+             "(" (substring-no-properties no-parenthesis nil function-name-pos)
+             (append result (nthcdr permutation-length args-string-list)
+                     '(")"))))))
+
 (provide 'grugru-utils)
 ;;; grugru-utils.el ends here
