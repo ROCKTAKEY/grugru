4ac2bf3877e3af7d23ace3165b9c8ed536ff4832
ROCKTAKEY <rocktakey@gmail.com>
2021-01-27 13:32:18 +0900

    fix: Out of range bug on the getter `char' is fixed.

--- grugru.el
+++ grugru.el
@@ -5,7 +5,7 @@
 ;; Author: ROCKTAKEY <rocktakey@gmail.com>
 ;; Keywords: convenience, abbrev, tools
 
-;; Version: 1.20.0
+;; Version: 1.20.1
 ;; Package-Requires: ((emacs "24.4"))
 ;; URL: https://github.com/ROCKTAKEY/grugru
 
@@ -329,7 +329,7 @@
   '((defun  . (bounds-of-thing-at-point 'defun))
     (symbol . (bounds-of-thing-at-point 'symbol))
     (word   . grugru--get-word)
-    (char   . (cons (point) (1+ (point))))
+    (char   . (unless (= (point) (point-max)) (cons (point) (1+ (point)))))
     (list   . (bounds-of-thing-at-point 'list))
     (line   . (bounds-of-thing-at-point 'line))
     (non-alphabet . grugru--get-non-alphabet)
